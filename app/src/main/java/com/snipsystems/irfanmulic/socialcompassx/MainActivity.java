dc4a7de7e61cf0f873503fddb0e4b71f95b5267e
Irfan Mulic <irfan.mulic@servicenow.com>
2017-04-12 09:46:57 -0700

    Initial commit

---
+++ app/src/main/java/com/snipsystems/irfanmulic/socialcompassx/MainActivity.java
@@ -0,0 +1,246 @@
+package com.snipsystems.irfanmulic.socialcompassx;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+
+    // define the display assembly compass picture
+    private ImageView image;
+
+    // record the compass picture angle turned
+    private float currentDegree = 0f;
+    private FirebaseAuth.AuthStateListener authListener;
+    private FirebaseAuth auth;
+    FirebaseDatabase database;
+
+    private ProgressBar progressBar;
+
+    private SensorManager mSensorManager;
+
+    TextView tvHeading;
+    private LocationManager mLocationManager;
+    private Location myGPSLocation;
+
+    List<Person> locationsTracked = new ArrayList<Person>();
+
+    Person myLocation = new Person("irfan", 32.65702666004866d, -116.9703197479248d, "San Diego");
+
+    private final LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(final Location location) {
+
+            myGPSLocation = location;
+            Log.i("IRFAN",location.toString());
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        database = FirebaseDatabase.getInstance();
+
+        populateDefaultLocations();
+        setContentView(R.layout.activity_main);
+
+        //get firebase auth instance
+        auth = FirebaseAuth.getInstance();
+
+        //get current user
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        TextView t = (TextView)findViewById(R.id.text1);
+        if (user != null)
+           t.setText(user.getEmail()+": "+user.getUid());
+
+        authListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user == null) {
+                    // user auth state is changed - user is null
+                    // launch login activity
+                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                    finish();
+                }
+            }
+        };
+
+        // our compass image
+        image = (ImageView) findViewById(R.id.imageViewCompass);
+
+        // TextView that will tell the user what degree is he heading
+        tvHeading = (TextView) findViewById(R.id.tvHeading);
+
+        // initialize your android device sensor capabilities
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2*60*1000,
+                10, mLocationListener);
+
+
+
+
+        final DatabaseReference myRef = database.getReference("lastGPSLocation");
+
+        final Handler h1 = new Handler();
+        Runnable pushCurrentLocation = new Runnable() {
+            @Override
+            public void run() {
+                if (myGPSLocation != null)
+                    myRef.setValue(myGPSLocation);
+                h1.postDelayed(this,10000);
+            }
+        };
+
+        h1.post(pushCurrentLocation);
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            public static final String TAG = "IRFAN";
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Location value = dataSnapshot.getValue(Location.class);
+                Log.d(TAG,"Value is : "+value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w(TAG, "Failed to read value.", databaseError.toException());
+            }
+        });
+    }
+
+    private void populateDefaultLocations() {
+        Person a = new Person("azra", 32.65702666004866d, -116.9703197479248d, "San Diego");
+        Person b = new Person("asja", 32.6570266600489d, -116.9703197479291d, "San Diego");
+
+        locationsTracked.add(a);
+        locationsTracked.add(b);
+
+        DatabaseReference ref = database.getReference("persons");
+        ref.child("locations").setValue(locationsTracked);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // for the system's orientation sensor registered listeners
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+                SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(authListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (authListener != null) {
+            auth.removeAuthStateListener(authListener);
+        }
+
+        // to stop the listener and save battery
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // to stop the listener and save battery
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+
+        // get the angle around the z-axis rotated
+        float degree = Math.round(event.values[0]);
+
+        tvHeading.setText("Heading: " + Float.toString(degree) + " degrees");
+
+        // create a rotation animation (reverse turn degree degrees)
+        RotateAnimation ra = new RotateAnimation(
+                currentDegree,
+                -degree,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF,
+                0.5f);
+
+        // how long the animation will take place
+        ra.setDuration(210);
+
+        // set the animation after the end of the reservation status
+        ra.setFillAfter(true);
+
+        // Start the animation
+        image.startAnimation(ra);
+        currentDegree = -degree;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+}
---
+++ app/src/main/java/com/snipsystems/irfanmulic/socialcompassx/Person.java
@@ -0,0 +1,60 @@
+package com.snipsystems.irfanmulic.socialcompassx;
+
+/**
+ * Created by irfan1 on 2/19/17.
+ */
+public class Person {
+    String name;
+    Double lon;
+    Double lat;
+    String place;
+
+    public Person(String name, Double lat, Double lon, String place) {
+        this.name = name;
+        this.lon = lon;
+        this.lat = lat;
+        this.place = place;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getLon() {
+        return lon;
+    }
+
+    public Double getLat() {
+        return lat;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLon(Double lon) {
+        this.lon = lon;
+    }
+
+    public void setLat(Double lat) {
+        this.lat = lat;
+    }
+
+    @Override
+    public String toString() {
+        return "com.snipsystems.irfanmulic.compassone.Person{" +
+                "name='" + name + '\'' +
+                ", lon='" + lon + '\'' +
+                ", lat='" + lat + '\'' +
+                ", place='" + place + '\'' +
+                '}';
+    }
+}
